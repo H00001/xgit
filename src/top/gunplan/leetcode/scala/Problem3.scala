6ca6e4d7ee183d5a90b01efd9eb33cc49904a5d9
frankhan <system-noreply@gunplan.top>
2020-08-03 10:35:18 +0800

    problem 3

---
+++ src/top/gunplan/leetcode/scala/Problem3.scala
@@ -0,0 +1,36 @@
+package top.gunplan.leetcode.scala
+
+import scala.util.control.Breaks._
+
+
+
+  object Solution {
+    def lengthOfLongestSubstring(s: String): Int = {
+      val dp = new Array[Int](s.length())
+      if (s.length() == 0 || s.length() == 1) {
+        return s.length()
+      }
+      dp(0) = 1
+
+      for (i <- 1 until s.length()) {
+        breakable {
+          for (j <- i - 1 until i - 1 - dp(i - 1) by -1) {
+
+            if (s.charAt(j) == s.charAt(i)) {
+              dp(i) = i - j
+              break
+            }
+          }
+          dp(i) = dp(i - 1) + 1
+        }
+      }
+      dp.maxBy(v=>{
+        v
+      })
+    }
+
+
+  }
+
+
+
